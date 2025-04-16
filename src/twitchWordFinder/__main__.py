584b6ec0f06cab1695e8837662c10bc8256139b8
The Crunching <theCrunching@proton.me>
2025-04-16 16:03:33 -0400

    Update __main__.py

--- src/twitchWordFinder/__main__.py
+++ src/twitchWordFinder/__main__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Twitch chat word finder!
 """
+
+import warnings
+import sys
 from twitch_chat_irc import twitch_chat_irc # Import Module
 
-Word = ""
 connection = twitch_chat_irc.TwitchChatIRC() # Start connection
 Channel = "mud_flaps123"
 def is_word(message):
@@ -12,13 +14,19 @@ def is_word(message):
     Args:
         message (_type_): The message object passed from listen().
     """
-    if message['message'].lower() == Word:
+    if Word in message['message'].lower().split():
         print(f"User {message['display-name']} guessed '{Word}'")
 
 def start():
-    global Word
-    Word = input("Please input word: ").lower()
-    connection.listen(Channel, on_message=is_word)
+    """Function to init the program.
+    """
+    if Word is None:
+        warnings.warn("Word must not be equal to 'None'.")
+        sys.exit(1)
+    connection.listen(Channel, on_message=is_word)# Start connection to chat
 
 if __name__ == "__main__":
-    start()
+    Word = input("Please input the word you would like to find: ")# Ask user for word
+    start()# Call start when script is loaded
+else:
+    Word = None# Define word just in case
