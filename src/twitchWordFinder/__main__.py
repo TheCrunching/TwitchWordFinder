8f5f67034cc5f85240ed9b40108293456db79419
The Crunching <theCrunching@proton.me>
2025-04-17 23:03:12 -0400

    Update __main__.py

--- src/twitchWordFinder/__main__.py
+++ src/twitchWordFinder/__main__.py
@@ -2,12 +2,33 @@
 """Twitch chat word finder!
 """
 
+import argparse
 from twitch_chat_irc import twitch_chat_irc # Import Module
+
+__version__ = "1.1.0rc1"
+
+parser = argparse.ArgumentParser(
+    prog="Twitch Chat Word Finder",
+    description="Finds a word in twitch chat.",
+    epilog="This is here for no reason.")
+
+parser.add_argument("-c", "--channel", default=None)
+parser.add_argument("-w", "--word", default=None)
+parser.add_argument("-v", "--version", action="version", version=__version__)
+
+args = parser.parse_args()
 
 connection = twitch_chat_irc.TwitchChatIRC() # Start connection
 
-channel = input("Please input the channel you would like to find a word in: ")
-word = input("Please input the word you would like to find: ")# Ask user for word
+if args.channel is None:
+    channel = input("Please input the channel you would like to find a word in: ")
+else:
+    channel = args.channel
+
+if args.word is None:
+    word = input("Please input the word you would like to find: ")# Ask user for word
+else:
+    word = args.word
 
 def is_word(message):
     """Check if user commented word.
@@ -22,7 +43,7 @@ def main():
     """Main function
     """
 
-    connection.listen(channel, on_message=is_word)# Start connection to chat
+    connection.listen(channel, on_message=is_word)# Start listening to chat
 
 if __name__ == "__main__":
     main()
